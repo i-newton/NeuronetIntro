903f611c474ea494f7aa930caf57e28f8881bc03
i-newton <gyddone@gmail.com>
2017-06-15 14:40:33 +0400

    Add convolutional net for mnist

--- keras/convo_net_sample.py
+++ keras/convo_net_sample.py
@@ -1,104 +1,98 @@
-
-'''Trains a simple deep NN on the MNIST dataset.
-Gets to 98.40% test accuracy after 20 epochs
-(there is *a lot* of margin for parameter tuning).
-2 seconds per epoch on a K520 GPU.
-'''
-
 import keras
-from keras.datasets import mnist
+import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
-
+from keras.layers import Dense, Dropout,Conv2D,MaxPooling2D, Flatten
+from keras.optimizers import Adadelta, RMSprop
+from keras import backend
+from numpy import genfromtxt
 
 batch_size = 128
 num_classes = 10
-epochs = 20
+epochs = 15
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+def read_training(filename):
+    my_data = genfromtxt(filename, delimiter=',', skip_header=1, dtype=np.float32)
+    labels = my_data[:, 0]
+    input_weights = my_data[:, 1:]
+    return input_weights, labels
 
-# convert class vectors to binary class matrices
+
+def read_test(filename):
+    return genfromtxt(filename, delimiter=',', skip_header=1, dtype=np.float32)
+
+x_train, y_train = read_training('train.csv')
 y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
+
+img_rows, img_cols = 28, 28
+
+if backend.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+"""
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation='softmax'))
+model.add(Conv2D(20, kernel_size=(5, 5),
+                 activation='sigmoid',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+model.compile(loss=keras.losses.mean_squared_error,
+              optimizer=Adadelta(),
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
-
-
-batch_size = 128
-num_classes = 10
-epochs = 20
-
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
+                    verbose=1)
+"""
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation='softmax'))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-model.summary()
-
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+                    verbose=1)
+
+x_test = read_test('test.csv')
+if backend.image_data_format() == 'channels_first':
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+else:
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+result_shape = (x_test.shape[0], 2)
+result = np.zeros(result_shape, dtype=np.int_)
+for i in range(result.shape[0]):
+    array = np.array([x_test[i]])
+    res = model.predict(array)[0]
+    max_val = 0
+    for detected in range(1, len(res)):
+        if res[detected] > res[max_val]:
+            max_val = detected
+    result[i][0] = i + 1
+    result[i][1] = max_val
+
+np.savetxt('result.csv', result, header='ImageId,Label', fmt='%d', delimiter=',', comments='')
+
